44a3a8c14383fbf01bd051ba48b36588089739b9
AlexTheProgrammer <alexander.business.s@gmail.com>
2014-02-05 15:28:06 +1100

    first commit

---
+++ gen/alextheprogrammer/com/github/gettingstarted/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package alextheprogrammer.com.github.gettingstarted;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
